f417395025236b48d42fee57f7b0d403f2096e3b
Samiylo Kryshu <skryshu@gmail.com>
2020-03-26 20:43:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  counter = 0
   highest = 100000
   
     name_hash.each do |key, value|
