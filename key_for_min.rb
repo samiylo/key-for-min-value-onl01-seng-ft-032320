417b62e3234f217c9305b552480ca33d7da1ab81
Samiylo Kryshu <skryshu@gmail.com>
2020-03-26 20:44:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,16 @@
 def key_for_min_value(name_hash)
   counter = 0
   highest = 100000
+  result = {}
   
     name_hash.each do |key, value|
       if value < highest
         highest = value
+        counter += 1 
+        
+        if counter == name_hash.length 
+          result[key] = value
+        
       end
     end
     name_hash[]
